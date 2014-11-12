d73a069327ec19825421a9e05b37fae5ab5834b5
Vijay Patel <vijay.patel@envivo.co.uk>
2014-11-12 11:56:51 +0000

    MemoryHelper no longer uses VisualBasic DLL to calculate system memory

--- Fresnel.Utils/MemoryHelper.cs
+++ Fresnel.Utils/MemoryHelper.cs
@@ -1,122 +1,88 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using Envivo.TrueView;
-using Microsoft.VisualBasic.Devices;
 
 namespace Envivo.Fresnel.Utils
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	internal class SystemInfo
-	{
-		internal SystemInfo()
-		{
-			this.DetermineRuntimeHost();
-		}
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MemoryHelper
+    {
+        private long _previousBytesInUse = GC.GetTotalMemory(true);
 
-		internal Computer Computer = new Computer();
-		internal ComputerInfo ComputerInfo = new ComputerInfo();
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern bool SetProcessWorkingSetSize(IntPtr handle, int minSize, int maxSize);
 
-		internal bool IsRunningUnderAspNet { get; private set; }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MEMORYSTATUSEX
+        {
+            public uint dwLength;
+            public uint dwMemoryLoad;
+            public ulong ullTotalPhys;
+            public ulong ullAvailPhys;
+            public ulong ullTotalPageFile;
+            public ulong ullAvailPageFile;
+            public ulong ullTotalVirtual;
+            public ulong ullAvailVirtual;
+            public ulong ullAvailExtendedVirtual;
 
-		internal bool IsRunningUnderWcf { get; private set; }
+            public MEMORYSTATUSEX()
+            {
+                this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            }
+        }
 
-		internal bool IsRunningOnClient { get; private set; }
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
 
-		internal bool IsRunningOnServer { get; private set; }
+        public MEMORYSTATUSEX MemoryStatus { get; private set; }
 
-		private void DetermineRuntimeHost()
-		{
-			var frameworkChecker = new NetFxVersionChecker();
+        public MemoryHelper()
+        {
+            this.MemoryStatus = new MEMORYSTATUSEX();
+            this.UpdateMemoryStatus();          
+        }
 
-			this.IsRunningUnderAspNet = System.Web.HttpContext.Current != null;
-			this.IsRunningUnderWcf = (frameworkChecker.IsInstalled(FrameworkVersion.Fx30) &&
-			(new WcfOperationContextChecker().IsAvailable()));
-			this.IsRunningOnClient = (!this.IsRunningUnderAspNet && !this.IsRunningUnderWcf);
-			this.IsRunningOnServer = !this.IsRunningOnClient;
-		}
+        public void UpdateMemoryStatus()
+        {
+            GlobalMemoryStatusEx(MemoryStatus);
+        }
 
-		internal void EmitSystemInformation(Preferences.UserPreferences preferences)
-		{
-			try
-			{
-				const long MEGABYTE = 1024 * 1024;
-				const short MINIMUM_FREE_MEMORY = 64;   // MB!
+        /// <summary>
+        /// Minimises the process' working set memory.
+        /// </summary>
+        /// <remarks>Use this to manage the application's memory usage</remarks>
+        public static void MinimiseWorkingSet()
+        {
+            SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, -1, -1);
+        }
 
-				long totalMemory = (long)this.ComputerInfo.TotalPhysicalMemory / MEGABYTE;
-				long freeMemory = (long)this.ComputerInfo.AvailablePhysicalMemory / MEGABYTE;
+        public void CollectMemory()
+        {
+            MinimiseWorkingSet();
 
-				long totalSwap = (long)this.ComputerInfo.TotalVirtualMemory / MEGABYTE;
-				long freeSwap = (long)this.ComputerInfo.AvailableVirtualMemory / MEGABYTE;
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // This allows us to monitor memory usage for the My.Application.
+                // NB: Each item in the MessageListView consumes about 1Kb, so that may be ignored.
 
-				Trace.TraceInformation("Application started by '{0}' on workstation '{1}'", Environment.UserName, Environment.MachineName);
+                for (int i = 1; i <= 3; i++)
+                {
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }
 
-				Trace.TraceInformation("Running on {0}{1}, {2}Mb RAM ({3}Mb free), {4}Mb swap ({5}Mb free)",
-					this.ComputerInfo.OSFullName.Trim(),
-					Environment.OSVersion.ServicePack.IsNotEmpty() ? " " + Environment.OSVersion.ServicePack.Trim() : "",
-					totalMemory, freeMemory, totalSwap, freeSwap);
+                long bytesInUse = GC.GetTotalMemory(true);
+                long difference = bytesInUse - _previousBytesInUse;
+                _previousBytesInUse = bytesInUse;
 
-				if (freeMemory < MINIMUM_FREE_MEMORY)
-				{
-					Trace.TraceWarning("This application requires at least {0}Mb to run adequately", MINIMUM_FREE_MEMORY);
-				}
+                var msg = string.Format("Current Memory: {0:#,###,##0} bytes ({1:+#,###,##0;-#,###,##0;0})", bytesInUse, difference);
+                System.Diagnostics.Debug.WriteLine(msg);
+            }
+        }
 
-				if (preferences.CommsSettings.CheckNetworkAtStartup)
-				{
-					if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
-					{
-						Trace.TraceError("A network connection is not available");
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				My.Instance.Engine.LogException(ex);
-			}
-		}
-
-
-
-		[DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
-		private static extern bool SetProcessWorkingSetSize(IntPtr handle, int minSize, int maxSize);
-
-
-		/// <summary>
-		/// Minimises the process' working set memory.
-		/// </summary>
-		/// <remarks>Use this to manage the application's memory usage</remarks>
-		internal static void MinimiseWorkingSet()
-		{
-			SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, -1, -1);
-		}
-
-		private long _previousBytesInUse = GC.GetTotalMemory(true);
-
-		internal void CollectMemory()
-		{
-			MinimiseWorkingSet();
-
-			if (System.Diagnostics.Debugger.IsAttached)
-			{
-				// This allows us to monitor memory usage for the My.Application.
-				// NB: Each item in the MessageListView consumes about 1Kb, so that may be ignored.
-
-				for (int i = 1; i <= 3; i++)
-				{
-					GC.Collect();
-					GC.WaitForPendingFinalizers();
-				}
-
-				long bytesInUse = GC.GetTotalMemory(true);
-				long difference = bytesInUse - _previousBytesInUse;
-				_previousBytesInUse = bytesInUse;
-
-				System.Diagnostics.Debug.WriteLine(string.Format("Current Memory: {0:#,###,##0} bytes ({1:+#,###,##0;-#,###,##0;0})", bytesInUse, difference));
-			}
-		}
-
-	}
+    }
 
 }
