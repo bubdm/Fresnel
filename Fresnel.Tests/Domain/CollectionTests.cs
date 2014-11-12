32b3f2742361346b240bf32fd8231aebd67d1070
Vijay Patel <vijay.patel@envivo.co.uk>
2014-11-12 11:26:29 +0000

    Added foundations for automated tests

---
+++ Fresnel.Tests/Domain/CollectionTests.cs
@@ -0,0 +1,28 @@
+﻿//  Author:
+//       Vijay Patel
+//
+// Copyright (c) 2014 Vijay Patel
+//
+using NUnit.Framework;
+using System;
+using Envivo.Fresnel.SampleModel.Objects;
+
+namespace Fresnel.Tests.Domain
+{
+    [TestFixture()]
+    public class CollectionTests
+    {
+        [Test()]
+        public void TestCase()
+        {
+            var source = new BiDirectionalExample();
+            var target = new BiDirectionalExample();
+
+            source.AddToContents(target);
+            Assert.IsTrue(source.Contents.Contains(target));
+            Assert.IsTrue(target.Contents.Contains(source));
+        }
+
+    }
+}
+
