cfab2a9b570da7f6e3f3f6fd58b28c52d3e12d70
Vijay Patel <vijay.patel@envivo.co.uk>
2014-11-20 07:01:53 +0000

    Added NullTemplate

---
+++ Fresnel.Introspection/Templates/Classes/NullTemplate.cs
@@ -0,0 +1,18 @@
+using System;
+using Envivo.Fresnel.Configuration;
+using Envivo.Fresnel.Utils;
+
+namespace Envivo.Fresnel.Introspection.Templates
+{
+
+    /// <summary>
+    /// A Template that represents a 'null' value
+    /// </summary>
+    public class NullTemplate : BaseClassTemplate
+    {
+        public NullTemplate()
+        {
+        }
+
+    }
+}
