0553a0f02b5740ffb782c191df3b1494fd4931ea
Vijay Patel <vijay.patel@envivo.co.uk>
2014-11-27 17:58:22 +0000

    CleaCollection now updates dirty status of affected objects

--- Fresnel.Core/Commands/ClearCollectionCommand.cs
+++ Fresnel.Core/Commands/ClearCollectionCommand.cs
@@ -6,28 +6,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Envivo.Fresnel.Utils;
+using Envivo.Fresnel.Core.ChangeTracking;
 
 namespace Envivo.Fresnel.Core.Commands
 {
     public class ClearCollectionCommand
     {
         private GetCollectionItemsCommand _GetCollectionItemsCommand;
+        private DirtyObjectNotifier _DirtyObjectNotifier;
 
-        public ClearCollectionCommand(GetCollectionItemsCommand getCollectionItemsCommand)
+        public ClearCollectionCommand
+            (
+            GetCollectionItemsCommand getCollectionItemsCommand,
+            DirtyObjectNotifier dirtyObjectNotifier
+            )
         {
             _GetCollectionItemsCommand = getCollectionItemsCommand;
+            _DirtyObjectNotifier = dirtyObjectNotifier;
         }
 
         public void Invoke(CollectionObserver oCollection)
         {
-            var items = _GetCollectionItemsCommand.Invoke(oCollection);
-            oCollection.UnbindItemsFromCollection(items);
+            var oItems = _GetCollectionItemsCommand.Invoke(oCollection);
+            oCollection.UnbindItemsFromCollection(oItems);
 
             var oMethod = oCollection.Methods.TryGetValueOrNull("Clear");
             if (oMethod != null)
             {
                 var tMethod = oMethod.Template;
                 tMethod.Invoke(oCollection.RealObject, null);
+
+                foreach (var oRemovedItem in oItems)
+                {
+                    _DirtyObjectNotifier.ObjectWasRemovedFromCollection(oRemovedItem, oCollection);
+                }
             }
         }
 
