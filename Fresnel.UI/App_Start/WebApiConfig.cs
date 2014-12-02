7a59e6b968a2c17d384a834cab6b64cb4a864c9e
Vijay Patel <vijay.patel@envivo.co.uk>
2014-12-02 13:41:43 +0000

    Corrections to ensure JSON data is returned from API calls

--- Fresnel.UI/App_Start/WebApiConfig.cs
+++ Fresnel.UI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Envivo.Fresnel.UI
@@ -14,12 +15,18 @@ namespace Envivo.Fresnel.UI
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // This allows non-standard REST methods to be invoked:
+            var routeTemplate = "api/{controller}/{action}/{id}";
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 //routeTemplate: "api/{controller}/{id}",
-                routeTemplate: "api/{controller}/{action}/{id}",
+                routeTemplate: routeTemplate,
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Make sure we return JSON from any API calls:
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
