49ec41d54e3f0e1345a0b56ae0b3d0f4cf985f7e
Vijay Patel <vijay.patel@envivo.co.uk>
2014-12-03 15:06:21 +0000

    Added equality IEntity equality checker

---
+++ Fresnel.Core/IEntityExtensions.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using Envivo.Fresnel.Utils;
+using Envivo.Fresnel.DomainTypes.Interfaces;
+
+namespace Envivo.Fresnel.Core
+{
+    public static class IEntityExtensions 
+    {
+ 
+        public static bool IsEqualTo(this IEntity a, IEntity b)
+        {
+            if (object.ReferenceEquals(a, b))
+                return true;
+
+            if (a == null && b == null)
+                return false;
+
+            if (a == null && b != null)
+                return false;
+
+            if (a != null && b == null)
+                return false;
+
+            if (a.GetType() == b.GetType())
+                return true;
+
+            if (a.ID == b.ID)
+                return true;
+
+            return false;
+        }
+
+
+    }
+}
