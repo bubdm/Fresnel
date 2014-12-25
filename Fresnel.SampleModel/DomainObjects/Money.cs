b2b2d88aa50831ae62f779902b82cdef01edcafa
Vijay Patel <vijay.patel@envivo.co.uk>
2014-12-25 06:14:20 +0000

    Now overrides Equals(), to prevent run-time Value object errors

--- Fresnel.SampleModel/DomainObjects/Money.cs
+++ Fresnel.SampleModel/DomainObjects/Money.cs
@@ -23,5 +23,20 @@ namespace Envivo.Fresnel.SampleModel.Objects
         /// </summary>
         public virtual string Description { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Money);
+        }
+
+        public virtual bool Equals(Money money)
+        {
+            if (money == null)
+                return false;
+
+            return string.Equals(this.Name, money.Name) &&
+                   string.Equals(this.Description, money.Description);
+        }
+
+
     }
 }
