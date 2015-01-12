4305fba19d8f7bb84cf2d619dfdc8479ce4753e4
Vijay Patel <vijay.patel@envivo.co.uk>
2015-01-12 11:13:21 +0000

    Now starts from zero (easier to debug!)

--- Fresnel.Core/SequentialIdGenerator.cs
+++ Fresnel.Core/SequentialIdGenerator.cs
@@ -6,7 +6,7 @@ namespace Envivo.Fresnel.Core
 {
     public static class SequentialIdGenerator
     {
-        private static long _ID = long.MinValue;
+        private static long _ID = 0;
 
         public static long Next
         {
