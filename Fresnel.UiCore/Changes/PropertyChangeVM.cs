0713ba03259cfddb0ab5050ff4a9a259d90be7d3
Vijay Patel <vijay.patel@envivo.co.uk>
2015-01-19 11:35:34 +0000

    Added Navigate, Create, Set, Add, and Remove InteractionPoint properties

--- Fresnel.UiCore/Changes/PropertyChangeVM.cs
+++ Fresnel.UiCore/Changes/PropertyChangeVM.cs
@@ -14,5 +14,30 @@ namespace Envivo.Fresnel.UiCore.Changes
         public object NonReferenceValue { get; set; }
 
         public Guid? ReferenceValueId { get; set; }
+
+        /// <summary>
+        /// State for navigation interaction
+        /// </summary>
+        public InteractionPoint Navigate { get; set; }
+
+        /// <summary>
+        /// Interaction state for creating new objects within the property
+        /// </summary>
+        public InteractionPoint Create { get; set; }
+
+        /// <summary>
+        /// Interaction state for setting the property to an object
+        /// </summary>
+        public InteractionPoint Set { get; set; }
+
+        /// <summary>
+        /// Interaction state for adding to a collection property
+        /// </summary>
+        public InteractionPoint Add { get; set; }
+
+        /// <summary>
+        /// Interaction state for removing from a collection property
+        /// </summary>
+        public InteractionPoint Remove { get; set; }
     }
 }
