f8d749857570cd6e1ea71caccfbfccc25eedda1a
Vijay Patel <vijay.patel@envivo.co.uk>
2015-01-19 17:26:19 +0000

    "1234" no longer converted to Date object in JSON

--- Fresnel.UI/AngularApp/Bootstrapper.ts
+++ Fresnel.UI/AngularApp/Bootstrapper.ts
@@ -1,8 +1,8 @@
 ﻿module FresnelApp {
 
     var requires = ['blockUI',
-                    'inform', 'inform-exception', 'inform-http-exception', 'ngAnimate',
-                    'smart-table'];
+        'inform', 'inform-exception', 'inform-http-exception', 'ngAnimate',
+        'smart-table'];
 
     angular.module("fresnelApp", requires)
         .service("appService", FresnelApp.AppService)
@@ -17,40 +17,41 @@
         .directive("aDisabled", FresnelApp.DisableAnchorDirective)
         .config(["$httpProvider", function ($httpProvider) {
             $httpProvider.defaults.transformResponse.push(function (responseData) {
-                convertDateStringsToDates(responseData);
-                return responseData;
-            })
-        }])
-        .config( function (blockUIConfig) {
-            blockUIConfig.message = 'Please wait...';
-            blockUIConfig.delay = 250;
-            blockUIConfig.resetOnException = true;
-        });
+            convertDateStringsToDates(responseData);
+            return responseData;
+        })
+    }])
+        .config(function (blockUIConfig) {
+        blockUIConfig.message = 'Please wait...';
+        blockUIConfig.delay = 250;
+        blockUIConfig.resetOnException = true;
+    });
 
-// Taken from http://aboutcode.net/2013/07/27/json-date-parsing-angularjs.html
-// TODO: Refactor the Date conversion into a self-contained object:
+    // See http://aboutcode.net/2013/07/27/json-date-parsing-angularjs.html
+    // and http://stackoverflow.com/a/3143231/80369
+    // TODO: Refactor the Date conversion into a self-contained object:
 
-var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+    var regexIso8601 = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/;
 
-function convertDateStringsToDates(input) {
-    // Ignore things that aren't objects.
-    if (typeof input !== "object") return input;
+    function convertDateStringsToDates(input) {
+        // Ignore things that aren't objects.
+        if (typeof input !== "object") return input;
 
-    for (var key in input) {
-        if (!input.hasOwnProperty(key)) continue;
+        for (var key in input) {
+            if (!input.hasOwnProperty(key)) continue;
 
-        var value = input[key];
-        var match;
-        // Check for string properties which look like dates.
-        if (typeof value === "string" && (match = value.match(regexIso8601))) {
-            var milliseconds = Date.parse(match[0])
-            if (!isNaN(milliseconds)) {
-                input[key] = new Date(milliseconds);
+            var value = input[key];
+            var match;
+            // Check for string properties which look like dates.
+            if (typeof value === "string" && (match = value.match(regexIso8601))) {
+                var milliseconds = Date.parse(match[0])
+                if (!isNaN(milliseconds)) {
+                    input[key] = new Date(milliseconds);
+                }
+            } else if (typeof value === "object") {
+                // Recurse into object
+                convertDateStringsToDates(value);
             }
-        } else if (typeof value === "object") {
-            // Recurse into object
-            convertDateStringsToDates(value);
         }
-    }
     }
 }
