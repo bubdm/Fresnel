9115f069e3a5d4f1d55d4f899092808875be0199
Vijay Patel <vijay.patel@envivo.co.uk>
2015-01-27 13:59:45 +0000

    Removed ellipsis from method name (UI concern)

--- Fresnel.Introspection/Templates/Methods/MethodTemplateBuilder.cs
+++ Fresnel.Introspection/Templates/Methods/MethodTemplateBuilder.cs
@@ -49,10 +49,6 @@ namespace Envivo.Fresnel.Introspection.Templates
                 result.IsVisible = false;
             }
 
-            // We're using GetParameters here, so that resul.Parameters doesn't get initialised prematurely:
-            if (result.MethodInfo.GetParameters().Any())
-                result.FriendlyName += "...";
-
             return result;
         }
     }
