0423e1c34a30c2998c0217478cd762f7f9ef2570
Vijay Patel <vijay.patel@envivo.co.uk>
2015-04-20 13:22:41 +0100

    Removed redundant ParentObjectID & ParentPropertyName

--- Fresnel.UiCore/Commands/CancelChangesRequest.cs
+++ Fresnel.UiCore/Commands/CancelChangesRequest.cs
@@ -6,10 +6,6 @@ namespace Envivo.Fresnel.UiCore.Commands
     [TypeScriptInterface]
     public class CancelChangesRequest
     {
-        public Guid ParentObjectID { get; set; }
-
-        public string ParentPropertyName { get; set; }
-
         /// <summary>
         /// The ID of the Object being cancelled
         /// </summary>
