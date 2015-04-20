4cdb5044ed64e6db13dd48ab353b2993c1e7851b
Vijay Patel <vijay.patel@envivo.co.uk>
2015-04-20 13:23:30 +0100

    Added dirty tracking tests for nested Objects vs nested Collections

--- Fresnel.Tests/Features/Explorer/Cancelling_Save_should_revert_affected_Objects.cs
+++ Fresnel.Tests/Features/Explorer/Cancelling_Save_should_revert_affected_Objects.cs
@@ -1,5 +1,7 @@
 ﻿using Autofac;
 using Envivo.Fresnel.CompositionRoot;
+using Envivo.Fresnel.Core.Observers;
+using Envivo.Fresnel.DomainTypes.Interfaces;
 using Envivo.Fresnel.SampleModel.Northwind;
 using Envivo.Fresnel.SampleModel.Objects;
 using Envivo.Fresnel.SampleModel.TestTypes;
@@ -8,7 +10,9 @@ using Envivo.Fresnel.UiCore.Controllers;
 using Envivo.Fresnel.UiCore.Model;
 using Envivo.Fresnel.Utils;
 using Fresnel.SampleModel.Persistence;
+using Fresnel.Tests;
 using NUnit.Framework;
+using Ploeh.AutoFixture;
 using System;
 using System.Linq;
 using System.Threading;
@@ -19,13 +23,14 @@ namespace Envivo.Fresnel.Tests.Features.Explorer
     [TestFixture()]
     public class Cancelling_Save_should_revert_affected_Objects
     {
+        private Fixture _Fixture = new AutoFixtureFactory().Create();
         private IContainer _Container;
         private ToolboxController _ToolboxController;
         private ExplorerController _ExplorerController;
 
         private SessionVM _Session;
-        private ObjectVM _ParentObject;
-        private ObjectVM _ChildObject;
+        private Order _orderPoco;
+        private ObjectVM _Order;
 
         public void Given_the_session_is_already_started()
         {
@@ -42,95 +47,100 @@ namespace Envivo.Fresnel.Tests.Features.Explorer
             _Session = sessionController.GetSession();
         }
 
-        public void when_a_domain_object_is_retrieved()
+        public void when_an_aggregate_root_exists_in_the_database()
         {
-            var searchRequest = new SearchObjectsRequest()
-            {
-                SearchType = typeof(Order).FullName,
-                PageSize = 10,
-                PageNumber = 1,
-            };
-            var searchResponse = _ToolboxController.SearchObjects(searchRequest);
-            Assert.IsTrue(searchResponse.Passed);
-            _ParentObject = searchResponse.Result.Items.First();
+            _orderPoco = _Fixture.Create<Order>();
+            _orderPoco.PlacedBy = _Fixture.Create<Employee>();
+            _orderPoco.PlacedBy.Address = _Fixture.Create<Address>();
+
+            var persistenceService = _Container.Resolve<IPersistenceService>();
+            persistenceService.SaveChanges(_orderPoco);
+
+            // This ensures the Object is being observed:
+            _Container.Resolve<ObserverCache>().GetObserver(_orderPoco);
+            _Container.Resolve<ObserverCache>().GetObserver(_orderPoco.PlacedBy);
+            _Container.Resolve<ObserverCache>().GetObserver(_orderPoco.PlacedBy.Address);
         }
 
-        public void And_when_a_child_object_is_added_to_the_parent()
+        public void when_the_aggregate_root_is_retrieved()
         {
-            var request = new CollectionAddNewRequest()
+            var getOrderRequest = new GetObjectRequest()
             {
-                ParentObjectID = _ParentObject.ID,
-                CollectionPropertyName = LambdaExtensions.NameOf<Order>(x => x.OrderItems),
-                ElementTypeName = typeof(OrderItem).FullName,
+                ObjectID = _orderPoco.ID
             };
-            var createAddResponse = _ExplorerController.AddNewItemToCollection(request);
-            Assert.IsTrue(createAddResponse.Passed);
+            var getOrderResponse = _ExplorerController.GetObject(getOrderRequest);
 
-            _ChildObject = createAddResponse.Modifications.NewObjects.Last();
+            Assert.IsNotNull(getOrderResponse.ReturnValue);
+            _Order = getOrderResponse.ReturnValue;
         }
 
-        public void And_when_the_child_object_is_modified()
+        public void And_when_the_Address_is_modified()
         {
             var request = new SetPropertyRequest()
             {
-                ObjectID = _ChildObject.ID,
-                PropertyName = LambdaExtensions.NameOf<OrderItem>(x => x.Quantity),
-                NonReferenceValue = 1234
+                ObjectID = _orderPoco.PlacedBy.Address.ID,
+                PropertyName = LambdaExtensions.NameOf<Address>(x => x.PostalCode),
+                NonReferenceValue = Guid.NewGuid().ToString()
             };
             var setResponse = _ExplorerController.SetProperty(request);
             Assert.IsTrue(setResponse.Passed);
         }
 
-        public void And_when_the_user_Cancels_the_Save()
+        public void And_when_the_user_Cancels_the_Address_changes()
         {
             var cancelRequest = new CancelChangesRequest()
             {
-                ParentObjectID = _ParentObject.ID,
-                ParentPropertyName = LambdaExtensions.NameOf<Order>(x => x.OrderItems),
-                ObjectID = _ChildObject.ID,
+                ObjectID = _orderPoco.PlacedBy.Address.ID,
             };
 
             var cancelResponse = _ExplorerController.CancelChanges(cancelRequest);
             Assert.IsTrue(cancelResponse.Passed);
         }
 
-        public void Then_the_original_object_should_not_have_dirty_contents()
+        public void Then_the_Order_should_not_have_dirty_contents()
         {
             var getRequest = new GetObjectRequest()
             {
-                ObjectID = _ParentObject.ID
+                ObjectID = _orderPoco.ID
             };
             var getResponse = _ExplorerController.GetObject(getRequest);
 
             Assert.IsNotNull(getResponse.ReturnValue);
 
-            var getPropertyRequest = new GetPropertyRequest()
-            {
-                ObjectID = _ParentObject.ID,
-                PropertyName = LambdaExtensions.NameOf<Order>(x => x.OrderItems),
-            };
-            var getPropertyResponse = _ExplorerController.GetObjectProperty(getPropertyRequest);
-
-            Assert.IsNotNull(getPropertyResponse.ReturnValue);
-
-            var collectionVM = (CollectionVM)getPropertyResponse.ReturnValue;
-            Assert.IsFalse(collectionVM.DirtyState.IsDirty);
-            Assert.IsFalse(collectionVM.DirtyState.HasDirtyChildren);
-
-            var matchingChild = collectionVM.Items.SingleOrDefault(i => i.ID == _ChildObject.ID);
-            Assert.IsNull(matchingChild);
+            var order = getResponse.ReturnValue;
+            Assert.IsFalse(order.DirtyState.IsDirty);
+            Assert.IsFalse(order.DirtyState.HasDirtyChildren);
         }
 
-        public void And_then_the_child_object_should_not_have_dirty_contents()
+        public void And_then_the_Employee_should_not_have_dirty_contents()
+        {
+            var getPropertyRequest = new GetPropertyRequest()
+            {
+                ObjectID = _orderPoco.ID,
+                PropertyName = LambdaExtensions.NameOf<Order>(x => x.PlacedBy),
+            };
+            var getPropertyResponse = _ExplorerController.GetObjectProperty(getPropertyRequest);
+            Assert.IsNotNull(getPropertyResponse.ReturnValue);
+
+            var employee = getPropertyResponse.ReturnValue;
+            Assert.IsFalse(employee.DirtyState.IsDirty);
+            Assert.IsFalse(employee.DirtyState.HasDirtyChildren);
+        }
+
+        public void And_then_the_Address_should_not_have_dirty_contents()
         {
             var getRequest = new GetObjectRequest()
             {
-                ObjectID = _ChildObject.ID
+                ObjectID = _orderPoco.PlacedBy.Address.ID
             };
             var getResponse = _ExplorerController.GetObject(getRequest);
 
-            var prop = getResponse.ReturnValue.Properties.Single(p=> p.InternalName == LambdaExtensions.NameOf<OrderItem>(x => x.Quantity));
-            Assert.AreNotEqual(1234, prop.State.Value);
+            var address = getResponse.ReturnValue;
+            Assert.IsFalse(address.DirtyState.IsDirty);
+            Assert.IsFalse(address.DirtyState.HasDirtyChildren);
+
+            var prop = address.Properties.Single(p => p.InternalName == LambdaExtensions.NameOf<Address>(x => x.PostalCode));
+            Assert.AreEqual(_orderPoco.PlacedBy.Address.PostalCode, prop.State.Value);
         }
 
         [Test]
