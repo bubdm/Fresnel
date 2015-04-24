4a6bf5f38b9cae6c4e9a42ae23b781c8c9d8b2db
Vijay Patel <vijay.patel@envivo.co.uk>
2015-04-24 17:19:48 +0100

    Employees collection is now initialised with empty List

--- Fresnel.SampleModel/Northwind/Places/Territory.cs
+++ Fresnel.SampleModel/Northwind/Places/Territory.cs
@@ -8,13 +8,15 @@ namespace Envivo.Fresnel.SampleModel.Northwind.Places
 {
     public class Territory
     {
+        private ICollection<Employee> _Employees = new List<Employee>();
+
         [Key]
         public Guid ID { get; set; }
 
         [ConcurrencyCheck]
         public long Version { get; set; }
 
-        public string Name{ get; set; }
+        public string Name { get; set; }
 
         public override string ToString()
         {
@@ -25,7 +27,11 @@ namespace Envivo.Fresnel.SampleModel.Northwind.Places
         /// The Employees allocated to this Territory
         /// </summary>
         [Relationship(Type = RelationshipType.Has)]
-        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<Employee> Employees
+        {
+            get { return _Employees; }
+            set { _Employees = value; }
+        }
 
         public void AddToEmployees(Employee employee)
         {
