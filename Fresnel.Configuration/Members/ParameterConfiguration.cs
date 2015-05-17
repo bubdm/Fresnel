b62041f3481f3a20c9784fe7b2d734c23f8df6ed
Vijay Patel <vijay.patel@envivo.co.uk>
2015-05-17 06:43:42 +0100

    Removed dead files

--- Fresnel.Configuration/Members/ParameterConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace Envivo.Fresnel.Configuration
-{
-    /// <summary>
-    /// Configuration for a method Parameter
-    /// </summary>
-    public class ParameterConfiguration : BaseConfiguration
-    {
-    }
-}
